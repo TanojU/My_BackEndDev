b8a73fadec9c5ded1acb4811238a7cc65eae7571
Tanoj <50785802+TanojU@users.noreply.github.com>
2022-11-25 10:26:06 +1100

    Add files via upload

---
+++ 1-Python_Coding_Lectures/20-Web_applications_with_Django-Using_Built_in_and_Custom_Applications/Create_SuperUser/myFirstProject/groceries_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def app_homepage(request):
+    return render(request, 'homepage.html')
+
+
+def about_us(request):
+    return render(request, "aboutUs.html")
+
+
+def services(request):
+    return render(request, "services.html")
+
+
+def contact_us(request):
+    return render(request, "contactUs.html")
