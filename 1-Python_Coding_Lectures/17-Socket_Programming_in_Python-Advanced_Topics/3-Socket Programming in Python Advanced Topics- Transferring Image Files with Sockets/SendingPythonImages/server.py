41258efb6de89274f666733fda406967e86e4c18
Tanoj <50785802+TanojU@users.noreply.github.com>
2022-11-25 10:15:45 +1100

    Add files via upload

---
+++ 1-Python_Coding_Lectures/17-Socket_Programming_in_Python-Advanced_Topics/3-Socket Programming in Python Advanced Topics- Transferring Image Files with Sockets/SendingPythonImages/server.py
@@ -0,0 +1,24 @@
+import socket
+from PIL import Image
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    s.bind((socket.gethostname(),4571))
+    s.listen(5)
+    print('Server is up. Listening for connections...\n')
+    
+    client, address = s.accept()
+    print('Connection to', address, 'established\n')
+    print('Client object:', client, '\n')
+
+    with open('server_files/dog.jpg','rb') as image_file:
+
+        image_batch = image_file.read(40960)
+
+        while (image_batch):
+            client.send(image_batch)
+            image_batch = image_file.read(40960)
+            print('One batch sent to client...')
+    
+print('Image sent successfully')
+
